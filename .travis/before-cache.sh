13c184936a07e0bffbaffdc0f488ab2ce422f331
Durgaprasad <durgaprasad.budhwani@gmail.com>
2017-08-09 17:08:11 +0530

    Removed cached react-native-azure-adal
    Removed cached react-native-azure-adal

--- .travis/before-cache.sh
+++ .travis/before-cache.sh
@@ -2,10 +2,10 @@
 
 case "${TRAVIS_OS_NAME}" in
   osx)
-    rm -rf example_tmp/node_modules/react-native-touch-id-android
+    rm -rf example_tmp/node_modules/react-native-azure-adal
   ;;
   linux)
-    rm -rf example_tmp/node_modules/react-native-touch-id-android
+    rm -rf example_tmp/node_modules/react-native-azure-adal
     rm -f $HOME/.gradle/caches/modules-2/modules-2.lock
   ;;
 esac
