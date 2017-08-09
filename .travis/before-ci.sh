37c46731517786798db4fcd0359b3b0c7514bd34
Durgaprasad <durgaprasad.budhwani@gmail.com>
2017-08-09 20:39:59 +0530

    avdmanager change command

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -6,7 +6,8 @@ case "${TRAVIS_OS_NAME}" in
   ;;
   linux)
     android list targets
-    echo "no" | android create avd --force -n test -t android-21 --abi armeabi-v7a --skin WVGA800 --device "Nexus 5" --sdcard 512M
+    echo "no" | android create avd --force -n test -t android-23 --abi armeabi-v7a --skin WVGA800 --device "Nexus 5" --sdcard 512M
+    echo "no" | avdmanager create avd --force -n test -t android-23 --abi armeabi-v7a --skin WVGA800 --device "Nexus 5" --sdcard 512M
     android list targets
     emulator -avd test -scale 96dpi -dpi-device 160 -no-audio -no-window &
     android-wait-for-emulator
