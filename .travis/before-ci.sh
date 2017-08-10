871b360e107b0345fdf68e31b96d2a63e1df4721
Durgaprasad <durgaprasad.budhwani@gmail.com>
2017-08-10 15:06:17 +0530

    Upgraded version

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -10,8 +10,8 @@ case "${TRAVIS_OS_NAME}" in
     touch "$ANDROID_SDK/licenses/android-sdk-license"
     echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_SDK/licenses/android-sdk-license"
     echo y | sdkmanager --verbose "system-images;android-23;google_apis;x86"
-    echo "no" | avdmanager avdmanager create avd -n test --abi armeabi-v7a --sdcard 512M -f -k "system-images;android-23;google_apis;x86"
-    echo "no" | android create avd -n test --abi armeabi-v7a --sdcard 512M -f -k "system-images;android-23;google_apis;x86"
+    echo "no" | avdmanager create avd -n test --abi armeabi-v7a --sdcard 512M -f -k "system-images;android-23;google_apis;x86"
+    echo "no" | create avd -n test --sdcard 512M -f -k "system-images;android-23;google_apis;x86"
     android list targets
     emulator -list-avds
     emulator -avd test -scale 96dpi -dpi-device 160 -no-audio -no-window &
