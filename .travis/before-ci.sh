3b503ad7e58b9ae06e8babeb13998074f6838a9c
Durgaprasad <durgaprasad.budhwani@gmail.com>
2017-09-06 09:57:02 +0530

    Change logic to create android image
    Change logic to create android image

--- .travis/before-ci.sh
+++ .travis/before-ci.sh
@@ -10,8 +10,7 @@ case "${TRAVIS_OS_NAME}" in
     touch "$ANDROID_SDK/licenses/android-sdk-license"
     echo -e "\n8933bad161af4178b1185d1a37fbf41ea5269c55" > "$ANDROID_SDK/licenses/android-sdk-license"
     echo y | sdkmanager --verbose "system-images;android-25;google_apis;x86"
-    echo "no" | avdmanager create avd -n test --sdcard 512M -f -k "system-images;android-25;google_apis;x86"
-    echo "no" | android create avd -n test --sdcard 512M -f -k "system-images;android-25;google_apis;x86"
+    echo "no" | android -v create avd -t "android-25" -n test --sdcard 512M -f
     android -list-avd
     emulator -list-avds
     emulator -avd test -scale 96dpi -dpi-device 160 -no-audio -no-window
